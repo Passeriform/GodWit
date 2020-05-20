b6afc563270efcb49f3879247eaf2a9004b6ae79
Passeriform <utbh23041999@gmail.com>
2020-05-20 08:39:07 +0530

    Utility: Add command parser macro.

---
+++ src/_utils/mod.rs
@@ -0,0 +1,9 @@
+#![macro_use]
+macro_rules! str_to_command {
+	// TODO: Handle errors
+	($command_str:expr, $args_vec:expr) => {{
+		let exec_vec = $command_str.split_whitespace();
+		$args_vec.extend(exec_vec.clone().skip(1).collect::<Vec<_>>());
+		exec_vec.collect::<Vec<_>>()[0].to_string()
+		}};
+}
