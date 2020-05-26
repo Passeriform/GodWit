49b37113e4436bd6efc95175d7d2b78f22373a92
Passeriform <utbh23041999@gmail.com>
2020-05-26 15:14:54 +0530

    CI: Add brew tap.

---
+++ pkg/brew/godwit-bin.rb
@@ -0,0 +1,23 @@
+class GodwitBin < Formula
+  version '0.1.1'
+  desc "A hackable yet sane project manager and automation suite."
+  homepage "https://github.com/Passeriform/GodWit"
+
+  if OS.mac?
+      url "https://github.com/Passeriform/GodWit/releases/download/#{version}/GodWit-#{version}-x86_64-apple-darwin.tar.gz"
+      sha256 "974351ca7d00083ba2fad52e2f2539c8ff114119c139420f592507962ab43b75"
+  elsif OS.linux?
+      url "https://github.com/Passeriform/GodWit/releases/download/#{version}/GodWit-#{version}-x86_64-unknown-linux.tar.gz"
+      sha256 "c6bba6d643b1a1f18994683e26d4d2b998b41a7a7360e63cb8ec9db8ffbf793c"
+  end
+
+  conflicts_with "godwit"
+
+  def install
+    bin.install "godwit"
+    man1.install "doc/godwit.1"
+
+    bash_completion.install "complete/godwit.bash"
+    zsh_completion.install "complete/godwit.zsh"
+  end
+end
